ba3f1c943db937fe7f21b38f8661a0fe5fdd8987
Jonas <jonasceolin@gmail.com>
2019-06-09 17:23:13 -0300

    script inicial

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { Pessoa } from 'src/app/models/pessoa.model';
+import { PessoaService } from './services/pessoa.service';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -8,9 +10,10 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
 	status = true;
-	constructor(private router: Router) { }
+	constructor(private pessoaService: PessoaService) { }
 
 	ngOnInit() {
+		this.adicionarPessoas();
 	}
 
 	changeClassSidenav() {
@@ -20,5 +23,27 @@ export class AppComponent {
 	logout() {
 		alert('Fazer logout!');
 	}
+
+	adicionarPessoas() {
+		if(this.pessoaService.getAll().length == 0) {
+			let pessoa: Pessoa = new Pessoa();
+			pessoa.nome = 'Jose da Silva';
+			pessoa.documento = '111.111.111-74';
+			pessoa.telefone = '(48) 9999-9999';
+			this.pessoaService.save(pessoa);
+
+			pessoa.nome = 'Marcos Antonio';
+			pessoa.documento = '222.222.222-73';
+			pessoa.telefone = '(48) 8888-8888';
+			this.pessoaService.save(pessoa);
+
+			pessoa.nome = 'Pedro Aguiar';
+			pessoa.documento = '333.333.333-72';
+			pessoa.telefone = '(48) 7777-7777';
+			this.pessoaService.save(pessoa);
+		}
+	}
+
+
 }
 
